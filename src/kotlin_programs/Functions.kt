2adb1d4dba7a15f84fa529da62c0bea26f1cc6cd
Akeel shafi <akeelshafi20@gmail.com>
2025-01-08 11:49:05 +0530

    Day 8 of Android

---
+++ src/kotlin_programs/Functions.kt
@@ -0,0 +1,43 @@
+package kotlin_programs
+
+fun main(){
+  //  task()
+  /*  val a = task2(10,20)
+    println(a)*/
+
+    println(task3())
+}
+
+/*
+ four types of functions
+1.function with no parameter and return type
+2.function with parameter and no return type
+3.function with no parameter and  return type
+4.function with parameter and return type        */
+
+// 1. function with no parameter and no return type
+
+/*
+fun task(){
+    val a = 10
+    val b = 20
+    val result = a+b
+    println(result)
+}*/
+
+// 2. function with parameter and no return type
+
+/*fun task2( a:Int, b:Int){
+   var c = a+b
+}*/
+
+// function with  no parameter and return
+
+/*fun task3():Int{
+    val a = 10
+    val b = 20
+    val c = a+b
+    return c
+
+
+}*/
