882aee7207a4829f3a52586bce46c29a62f011bc
Akeel shafi <akeelshafi20@gmail.com>
2025-01-08 12:06:59 +0530

    Day 8 of Android

---
+++ src/kotlin_programs/DailyTask.kt
@@ -0,0 +1,30 @@
+package kotlin_programs
+
+class DailyTask {
+
+    fun wakeUp(){
+        println("Waking up in the morning")
+    }
+    fun bath(){
+        println("Taking bath...")
+    }
+    fun ready(){
+        println("Getting ready...")
+    }
+    fun study(){
+        println("studying in college")
+    }
+    fun backToHome(){
+        println("Back to Home")
+    }
+
+}
+fun main(){
+  val  akeel = DailyTask()
+    akeel.wakeUp()
+    akeel.bath()
+    akeel.ready()
+    akeel.study()
+    akeel.backToHome()
+
+}
